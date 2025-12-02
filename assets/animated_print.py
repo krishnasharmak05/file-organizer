11fa43a995fe320bfc943c10075e8705978ea3ec
krishnasharmak05 <krishnasharmak05@protonmail.com>
2025-12-03 00:41:52 +0530

    chore: Improved AnimatedPrint to a better working-but-barely code.

--- assets/animated_print.py
+++ assets/animated_print.py
@@ -1,29 +1,33 @@
 import time
+from typing import TextIO
 
-class AnimatedPrint:
-    def __text_generator(text):
+
+# TODO: Redo all/most of it, as required based on the fact that it should now be producing a stream,
+# that animatedly prints to console.
+class AnimatedPrint(TextIO):
+    @classmethod
+    def __text_generator(cls, text: str):
         for i in text:
             yield i
-    
+
     @classmethod
-    def animated_print(self, text):
-        generated_text = self.__text_generator(text)
+    def animated_print(cls, text):
+        generated_text = cls.__text_generator(text)
         try:
             while True:
                 print(next(generated_text), end="", flush=True)
                 time.sleep(0.05)
         except StopIteration:
             print()
-            return None
-        
-    
+            # return None
+
     @classmethod
-    def progress_indicator(self, percentage: int):
+    def progress_indicator(cls, percentage: int):
         completed = "▓"
         remaining = "░"
 
         ## Work from here...Not Complete.
-        
+
 
 if __name__ == "__main__":
     AnimatedPrint.animated_print("Hello, World!")
