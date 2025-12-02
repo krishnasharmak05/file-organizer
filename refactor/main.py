4a86259b4549ad92cdbf76855d8267a56cb18cce
krishnasharmak05 <krishnasharmak05@protonmail.com>
2025-12-03 00:46:51 +0530

    feat: Add partial main file.

--- refactor/main.py
+++ refactor/main.py
@@ -1,5 +1,38 @@
-from refactor.file_organizer import FileOrganizer
+import sys
+import time
+from pathlib import Path
+
+from customtkinter import filedialog
+
+from assets.animated_print import AnimatedPrint
+from refactor.FileOrganizer import FileOrganizer
+
+
+def get_folder()->str:
+    initial_path: Path = Path.home() / "Downloads"
+    if not initial_path.exists():
+        initial_path = Path.home()
+    folder: str = filedialog.askdirectory(
+        initialdir=initial_path, mustexist=True, title="Select a folder to sort"
+    )
+    if not folder:
+        AnimatedPrint.animated_print("No folder selected. Exiting safely...")
+        time.sleep(2)
+        sys.exit(0)
+    return folder
+
+
+def get_extension_map():
+    # TODO: Do this after completing the Classifier class.
+    return {}
+
+
+def main():
+    folder = get_folder()
+    extension_map = get_extension_map()
+    engine = FileOrganizer(folder, extension_map)
+    engine.organize()
 
 
 if __name__ == "__main__":
-    engine = FileOrganizer()
+    main()
